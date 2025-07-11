ad1274090e542664360522c9b5cab57893dcccb5
Juan Carlos Moreno Guerra <juan.moreno@designingsolutions.com.co>
2025-07-11 13:47:35 -0500

    cambioos

---
+++ src/components/CustomComponents/MasivesFiles/MainView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import FileUploadModal from './FileUploadModal.vue'
+
+const showUploadModal = ref(false)
+
+const openUploadModal = () => {
+  showUploadModal.value = true
+}
+</script>
+
+<template>
+  <div>
+    <VBtn prepend-icon="tabler-upload" @click="openUploadModal" elevation="2">
+      Start Upload
+    </VBtn>
+
+    <FileUploadModal v-model="showUploadModal" :maxFileSizeMB="30" :allowed-extensions="['.jpg', '.png', '.pdf']"
+      :max-files="5" />
+  </div>
+</template>
--- src/pages/second-page.vue
+++ src/pages/second-page.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import ListTables from "@/pages/Redis/Components/ListTables.vue";
 
 </script>
 
 <template>
   <div>
-    <!-- <ValidJSONSchema /> -->
-    <ListTables></ListTables>
+    <MainView />
+
   </div>
 </template>
